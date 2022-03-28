8132ade38b1e96df75057523ffcb6aecf00e1866
Rifky Mcz <mczxcyber@gmail.com>
2022-03-29 02:19:41 +0700

    RMX1921: Bringup ProtonAOSP

--- RMX1921.mk
+++ RMX1921.mk
@@ -4,19 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# GMS
+WITH_GMS := true
+
+# Device identifier
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := evolution_RMX1921
+PRODUCT_NAME := RMX1921
 PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
